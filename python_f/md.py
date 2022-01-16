e4ea2c3850d180742fb4e1a574b7a94f95e1ef20
tom <tom.young@chem.ox.ac.uk>
2022-01-16 12:12:22 +0000

    Refactor functional Python

--- python_f/md.py
+++ python_f/md.py
@@ -14,7 +14,7 @@ def _checked_positive(value: Any) -> Any:
     return value
 
 
-def _read_xyz_file_to_flat_array(filename):
+def _read_xyz_file_to_flat_array(filename: str):
     """Read vectors from a file with lines x, y, z.. on each line, Generates
     a row-major flat array with form (x0, y0, z0, x1, y1, ...)"""
 
@@ -65,7 +65,7 @@ def calculate_lj_force_coefficients(epsilon: float,
     return [epsilon / 2.0, 12 * sigma ** 12, -6 * sigma ** 6]
 
 
-def _calculate_forces(positions:          List[float],
+def _calculate_forces(r:                  List[float],
                       force_function:     Callable,
                       force_coefficients: List[float]
                       ) -> List[float]:
@@ -73,16 +73,16 @@ def _calculate_forces(positions:          List[float],
 
     forces = []
 
-    n_particles = len(positions) // 3
+    n_particles = len(r) // 3
 
     for i in range(n_particles):
 
-        pos_i = positions[3*i:3*i + 3]
+        pos_i = r[3 * i:3 * i + 3]
         force = [0.0, 0.0, 0.0]   # Zero initial force on particle i
 
         for j in range(n_particles):
 
-            pos_j = positions[3*j:3*j + 3]
+            pos_j = r[3 * j:3 * j + 3]
             fx, fy, fz = force_function(pos_i,
                                         pos_j,
                                         force_coefficients)
@@ -95,78 +95,77 @@ def _calculate_forces(positions:          List[float],
     return forces
 
 
-def _update_positions(positions, velocities, accelerations, dt) -> None:
+def _update_positions(r:  List[float],
+                      v:  List[float],
+                      a:  List[float],
+                      dt: float
+                      ) -> None:
     """Update positions using velocities and accelerations in place"""
 
-    n_particles = len(positions) // 3
+    n_particles = len(r) // 3
 
     for i in range(n_particles):
         for k in range(3):           # x, y, z
 
             idx = 3*i + k
-            positions[idx] += velocities[idx] * dt + accelerations[idx] * dt**2
+            r[idx] += v[idx] * dt + a[idx] * dt ** 2
 
     return None
 
 
-def _update_velocities(velocities, new_acceleration, accelerations, dt) -> None:
+def _update_velocities(v:     List[float],
+                       new_a: List[float],
+                       a:     List[float],
+                       dt:    float
+                       ) -> None:
     """Update velocities using current and new accelerations in place"""
 
-    n_particles = len(velocities) // 3
+    n_particles = len(v) // 3
 
     for i in range(n_particles):
         for k in range(3):  # x, y, z
 
             idx = 3 * i + k
-            velocities[idx] += (new_acceleration[idx] + accelerations[idx]) * (dt / 2.0)
+            v[idx] += (new_a[idx] + a[idx]) * (dt / 2.0)
 
     return None
 
 
-def _force_to_acceleration(forces) -> List[float]:
+def _force_to_acceleration(f: List[float]
+                           ) -> List[float]:
     """Calculate the acceleration using F = ma"""
     mass = 1.0
 
-    return [f/mass for f in forces]
+    return [f_k / mass for f_k in f]
 
 
-def run_md(positions:          List[float],
-           velocities:         List[float],
+def run_md(r:                  List[float],
+           v:                  List[float],
            force_function:     Callable,
            force_coefficients: List[float],
            n_steps:            int,
            timestep:           float
-           ) -> List[float]:
-    """Run velocity verlet molecular dynamics (MD)"""
+           ) -> None:
+    """Run velocity verlet molecular dynamics (MD), updating positions and
+    velocities in place"""
 
-    forces = _calculate_forces(positions, force_function, force_coefficients)
+    f = _calculate_forces(r, force_function, force_coefficients)
+    dt = _checked_positive(timestep)
 
     for _ in range(_checked_positive(n_steps)):
 
-        accelerations = _force_to_acceleration(forces)
+        a = _force_to_acceleration(f)
+        _update_positions(r, v, a, dt)
+        new_f = _calculate_forces(r, force_function, force_coefficients)
+        new_a = _force_to_acceleration(new_f)
+        _update_velocities(v, new_a, a, dt)
 
-        _update_positions(positions,
-                          velocities,
-                          accelerations,
-                          timestep)
+        f = new_f
 
-        new_forces = _calculate_forces(positions,
-                                       force_function,
-                                       force_coefficients)
-
-        new_accelerations = _force_to_acceleration(new_forces)
-
-        _update_velocities(velocities,
-                           new_accelerations,
-                           accelerations,
-                           timestep)
-
-        forces = new_forces
-
-    return positions
+    return None
 
 
-def print_txt_file(array: List[float],
+def print_txt_file(array:    List[float],
                    filename: str
                    ) -> None:
     """Print a .txt file from a flat array in the shape Nx3"""
@@ -185,13 +184,14 @@ if __name__ == '__main__':
 
     coeffs = calculate_lj_force_coefficients(epsilon=100, sigma=1.7)
 
-    final_positions = run_md(
-                   positions=read_positions(filename='data/positions.txt'),
-                   velocities=read_velocities(filename='data/velocities.txt'),
-                   force_function=ljpairwise_force,
-                   force_coefficients=coeffs,
-                   n_steps=10000,
-                   timestep=0.01
-                   )
+    positions = read_positions(filename='data/positions.txt')
+    velocities = read_velocities(filename='data/velocities.txt')
 
-    print_txt_file(final_positions, filename='positions.txt')
+    run_md(r=positions,
+           v=velocities,
+           force_function=ljpairwise_force,
+           force_coefficients=coeffs,
+           n_steps=10000,
+           timestep=0.01)
+
+    print_txt_file(positions, filename='positions.txt')
