85a094e920a3f65e2f471e50a8ac0ed7906aa0cf
tom <tom.young@chem.ox.ac.uk>
2022-01-19 21:24:50 +0000

    How to static cast

--- cpp_oo/md.cpp
+++ cpp_oo/md.cpp
@@ -8,6 +8,7 @@
 #include <fstream>
 #include <iostream>
 #include <stdexcept>
+#include <math.h>
 
 
 using namespace std;
@@ -28,32 +29,62 @@ vector<string> split(const string &s, char delim) {
 }
 
 
+template <class T>
 class Vector3D : public array<double, 3>{
     // A vector in 3D space with x, y, z components
 
     public:
+
         double x() const {return at(0);}
         double y() const {return at(1);}
         double z() const {return at(2);}
 
+        T operator/(double scalar) const {
+            // Overloaded divide operator
+            return {at(0)/scalar, at(1)/scalar, at(2)/scalar};
+        }
 
+        T operator*(double scalar) const {
+            // Overloaded divide operator
+            return {at(0)*scalar, at(1)*scalar, at(2)*scalar};
+        }
+
+
+    friend ostream &operator<<(std::ostream &os, Vector3D const &vec) {
+        // Overloaded << operator for printing a vector
+        return os << '(' << vec.x() << ", " << vec.y() << ", " << vec.z() << ')';
+    }
 };
 
-ostream &operator<<(std::ostream &os, Vector3D const &vec) {
-    // Overloaded << operator for printing vector
-    return os << '(' << vec.x() << ", " << vec.y() << ", " << vec.z() << ')';
+
+class Position: public Vector3D<Position>{};
+class Velocity: public Vector3D<Velocity>{};
+class Acceleration: public Vector3D<Acceleration>{};
+
+
+class Force: public Vector3D<Force>{
+
+    public:
+        void zero(){
+            // Zero the force vector
+            this->at(0) = 0.0;
+            this->at(1) = 0.0;
+            this->at(2) = 0.0;
+        }
+};
+
+
+double checked_positive(double x){
+    assert(x > 0);
+    return x;
 }
-
-class Position: public Vector3D{};
-class Velocity: public Vector3D{};
-
-class Force: public Vector3D{};
 
 
 class Particle{
     // Particle with a defined positions, velocity and force
 
     public:
+        double mass = 1.0;
         Position position = {0.0, 0.0, 0.0};
         Velocity velocity = {0.0, 0.0, 0.0};
         Force force = {0.0, 0.0, 0.0};
@@ -64,6 +95,66 @@ class Particle{
 
             this->position = {x, y, z};
         }
+
+        void update_position(double dt){
+            // Update the position based on a velocity verlet
+            position += (static_cast<Position>(velocity * dt)
+                        + static_cast<Position>(a() * dt*dt));
+        }
+
+        friend bool operator==(const Particle& lhs, const Particle& rhs){
+            // Equality pf particles is defined by equality of references
+            return &lhs == &rhs;
+        }
+
+    protected:
+
+        Vector3D<Force> a(){
+            // Acceleration due to the current force on the particle (F = ma)
+            return force / mass;
+        }
+
+        Vector3D<Force> a_prev(){
+            // Acceleration due to the previous force on the particle
+            return prev_force / mass;
+        }
+
+};
+
+
+class LJPotential{
+
+public:
+    LJPotential(double epsilon, double sigma){
+        // Initialise a Lennard Jones potential from ε and σ
+
+        f[0] = epsilon / 2.0;
+        f[1] = 12 * pow(sigma, 12);
+        f[2] = -6 * pow(sigma, 6);
+    }
+
+    void add_force(Particle &particle_i,
+                   Particle &particle_j){
+        // Add the force on particle i due to particle j
+
+        if (particle_i == particle_j) return;  // No self interaction
+
+        double dx = particle_i.position.x() - particle_j.position.x();
+        double dy = particle_i.position.y() - particle_j.position.y();
+        double dz = particle_i.position.z() - particle_j.position.z();
+        double r = sqrt(dx * dx + dy * dy + dz * dz);
+
+        double c = f[0] * (f[1] * pow(r, -14) + f[2] * pow(r, -8));
+
+        particle_j.force[0] += c * dx;
+        particle_j.force[1] += c * dy;
+        particle_j.force[2] += c * dz;
+    }
+
+protected:
+    // Coefficients used to calculate the gradient
+    array<double, 3> f = {0.0, 0.0, 0.0};
+
 };
 
 
@@ -81,7 +172,7 @@ class Particles : public vector<Particle>{
             // Set velocities from a file of x,y,z velocities
             int i = 0;
 
-            for (auto &vec : matrix_from_filename(filename)){
+            for (auto &vec : matrix_from(filename)){
 
                 if (i > this->size()){
                     throw runtime_error("Cannot set velocity for particle");
@@ -92,16 +183,30 @@ class Particles : public vector<Particle>{
             }
         }
 
+        void calculate_forces(LJPotential &potential){
+            // Calculate the forces on all particles
+
+            for (auto &particle_i : *this){
+
+                particle_i.prev_force = particle_i.force;
+                particle_i.force.zero();
+
+                for (auto &particle_j: *this){
+                    potential.add_force(particle_i, particle_j);
+                }
+            }
+        }
+
     protected:
         void add_particles(const string& filename){
             // Add particles at defined positions from a file
 
-            for (auto &vec : matrix_from_filename(filename)){
+            for (auto &vec : matrix_from(filename)){
                 emplace_back(vec[0], vec[1], vec[2]);
             }
         }
 
-        static vector<array<double, 3>> matrix_from_filename(const string& filename){
+        static vector<array<double, 3>> matrix_from(const string& filename){
             // Extract a Nx3 matrix of strings from a file
 
             string line;
@@ -130,11 +235,14 @@ class Particles : public vector<Particle>{
 };
 
 
+
 int main(){
 
     // TODO: remove hard coded paths
     auto cluster = Particles("/Users/tom/repos/md-comparison/data/positions.txt");
     cluster.set_velocities("/Users/tom/repos/md-comparison/data/velocities.txt");
+
+    auto potential = LJPotential(100.0, 1.7);
 
     return 0;
 }
