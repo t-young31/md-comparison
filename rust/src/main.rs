9bb1bad761e9bbf293965fd020113c41830bfacb
tom <tom.young@chem.ox.ac.uk>
2022-01-23 18:09:25 +0000

    Start rust impl

---
+++ rust/src/main.rs
@@ -0,0 +1,24 @@
+
+pub struct Particle{
+
+    position:   [f64; 3],
+    velocity:   [f64; 3],
+    force:      [f64; 3],
+    prev_force: [f64; 3],
+
+}
+
+
+
+pub struct Particles{
+
+
+    _list: Vec<Particle>
+
+}
+
+
+fn main() {
+    // Nothing
+}
+
